ede7f53f89e50ef92ca14a2a5b1a4d3209cf5faf
SarthakPradhan <40028495+SarthakPradhan@users.noreply.github.com>
2021-11-18 14:41:36 -0500

    Update DFS.py

--- Searching/Graph_Algorithms/DFS.py
+++ Searching/Graph_Algorithms/DFS.py
@@ -31,3 +31,31 @@ def BFS(graph, start_node):
             queue.append(n)
 
 BFS(graph,'a')
+
+print('has path using DFS recursion')
+#check if there is a path using depth first recursion
+def hasPath_DF(graph,node,goal):
+    if node == goal: return True
+    neighbours = graph[node]
+    for n in neighbours:
+        if hasPath_DF(graph,n,goal):
+            return True
+
+    return False
+
+print(hasPath_DF(graph,'a','e'))
+print(hasPath_DF(graph,'a','k'))
+
+print('has path using BFS recursion')
+#check if there is a path using depth first recursion
+def hasPath_BF(graph,node,goal):
+    queue = [node]
+    
+    while queue:
+        neighbours = graph[queue.pop(0)]
+        for n in neighbours:
+            if n==goal: return True
+            queue.append(n)
+    return False
+print(hasPath_BF(graph,'a','k'))
+print(hasPath_BF(graph,'a','e'))
